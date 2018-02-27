7146409e49cfdf5ff02fc8902db6d8e2a42014a3
Himanshu <himanshu@Himanshus-MacBook-Air.local>
2018-02-27 16:45:39 +0530

    Responsive dot numbers.

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 var p = [];
 var canvas;
+var total;
 function setup()
 {
 	canvas = createCanvas(windowWidth, windowHeight);
 	background(255);
-	total = 30;
-	for(let i = 0; i < total; i++)
+	total = (canvas.height * canvas.width) / 38000;
+	for(let i = 0; i < 150; i++)
 	{
 		p.push(new particle());
 	}
@@ -30,6 +31,7 @@ window.onresize = function() {
 	var w = window.innerWidth;
 	var h = window.innerHeight;
 	canvas.size(w,h);
+	total = (canvas.height * canvas.width) / 38000;
   width = w;
   height = h;
 }
